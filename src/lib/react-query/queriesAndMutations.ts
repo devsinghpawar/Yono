1749044c82c96983cf128d45a8fb95fcc4dff512
Dev singh pawar <pawarsinghdev@gmail.com>
2023-11-06 16:38:01 +0530

    Update PostForm.tsx

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -138,6 +138,19 @@ export const useGetPostById = (postId: string) => {
     enabled: !!postId,
   });
 };
+
+export const useUpdatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: IUpdatePost) => updatePost(post),
+    onSuccess: (data) => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_POST_BY_ID, data?.$id],
+      });
+    },
+  });
+};
+
 export const useDeletePost = () => {
   const queryClient = useQueryClient();
 
